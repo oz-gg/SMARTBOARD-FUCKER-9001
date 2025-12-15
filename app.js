f0d802096fa8ca8749532cf024dfe63b8a662585
oz-gg <160617598+oz-gg@users.noreply.github.com>
2025-12-15 19:26:05 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const KEY = "Sdal.325600+";
+
+const output = document.getElementById("output");
+const startBtn = document.getElementById("scan");
+
+let qrScanner;
+
+startBtn.addEventListener("click", () => {
+	if (!qrScanner) {
+		qrScanner = new Html5Qrcode("reader");
+	}
+
+	qrScanner.start(
+		{ facingMode: "environment" },
+		{ fps: 10, qrbox: 250 },
+		onScanSuccess,
+		onScanError
+	);
+});
+
+function decrypt(text) {
+	let hash = CryptoJS.MD5(text + password + "t%342!!vgj..")
+		.toString(CryptoJS.enc.Hex);
+
+	hash = hash.substring(0, 4);
+
+	hash = hash
+		.replace(/a/g, "9")
+		.replace(/b/g, "8")
+		.replace(/c/g, "7")
+		.replace(/d/g, "6")
+		.replace(/e/g, "5")
+		.replace(/f/g, "4");
+
+	return hash;
+}
+
+function onScanSuccess(decodedText) {
+	qrScanner.stop();
+
+	output.textContent = decrypt(decodedText);
+}
+
+function onScanError(_) {}
+
+if ("serviceWorker" in navigator) {
+	navigator.serviceWorker.register("sw.js");
+}
---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "qr-md5-v1";
+const FILES = [
+	"./",
+	"./index.html",
+	"./app.js",
+	"./manifest.json",
+	"./libs/html5-qrcode.min.js",
+	"./libs/crypto-js.js"
+];
+
+self.addEventListener("install", e => {
+	e.waitUntil(
+		caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+	);
+});
+
+self.addEventListener("fetch", e => {
+	e.respondWith(
+		caches.match(e.request).then(r => r || fetch(e.request))
+	);
+});
